f0924ac77e29733bd51088d16ef7a7cfe00bedf8
Unknown <rayjuanmartin@gmail.com>
2017-04-21 21:43:55 -0300

    diseño header y body

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>home</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <meta name="viewport" content="width = device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <div class="logo">
+          <img src="" alt="">
+          <h1>ACA VA EL LOGO</h1>
+        </div>
+        <div class="usuario">
+          <img src="" alt="">
+          ACA VA EL USUARIO
+        </div>
+      </nav>
+
+    </header>
+    <div class="contenedor">
+      <div class="contenedor_rubros">
+        <div class="rubro">
+          RUBRO1
+        </div>
+        <div class="rubro">
+          RUBRO2
+        </div>
+        <div class="rubro">
+          RUBRO3
+        </div>
+        <div class="rubro">
+          RUBRO4
+        </div>
+        <div class="rubro">
+          RUBRO5
+        </div>
+        <div class="rubro">
+          RUBRO6
+        </div>
+      </div>
+        <div class="contenedor_lateral">
+          <div class="ingreso">
+            <button type="submit" name="ingresar">Ingresar</button>
+          </div>
+        </div>
+      </div>
+
+  </body>
+</html>
